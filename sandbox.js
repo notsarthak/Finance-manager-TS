bd3045fb2b5636b1aba897e6b9db81cdaeb3590a
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-05-19 14:18:22 +0530

    Boilerplate html, css. Ts compilation and first line of code

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+var mario = 'mario';
+console.log(mario);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const mario = 'mario';
+
+console.log(mario);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach(input => console.log(input));
