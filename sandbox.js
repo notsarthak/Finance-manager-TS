968af521278d9dba242af5cf72586f1b3cff2c4f
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-05-20 11:15:41 +0530

    Explicit type definition, union type

--- sandbox.js
+++ sandbox.js
@@ -13,46 +13,63 @@
 // console.log(circumference(7.5));
 //4
 //ARRAYS
-var names = ["luigi", "mario", "ken"];
-// names.push(2);
-// names[0] = 3
-// names = ""
-names.push("powerup");
-names[1] = "ryu";
-// names = [1,2,3,4];
-var numbers = [1, 2, 3];
-// numbers.push("ryu");
-// numbers[1] = "ken";
-numbers.push(4);
-numbers[2] = 5;
-var mixed = ["ken", 1, "luigi", 2, 3, "jung-li"];
-mixed.push("ken");
-mixed[0] = 0;
-mixed.push(5);
+// let names = ["luigi", "mario", "ken"];
+// // names.push(2);
+// // names[0] = 3
+// // names = ""
+// names.push("powerup");
+// names[1] = "ryu";
+// // names = [1,2,3,4];
+// let numbers = [1,2,3];
+// // numbers.push("ryu");
+// // numbers[1] = "ken";
+// numbers.push(4);
+// numbers[2] = 5
+// let mixed = ["ken", 1, "luigi", 2, 3, "jung-li"];
+// mixed.push("ken");
+// mixed[0] = 0;
+// mixed.push(5);
 //Objects
-var ninja = {
-    name: "Yoshi",
-    belt: "Black",
-    age: 30
-};
-// ninja.age = "30";
-ninja.age = 30;
-ninja.name = "Ryu";
-// ninja.skills = ["fighting"];
-// ninja = [];
-// ninja = "";
-// ninja = {
-//     name: "Ryu",
-//     belt: "Orange"
-// }
-ninja = {
-    name: "Ryu",
-    belt: "Yellow",
-    age: 25
-};
+// let ninja = {
+//     name: "Yoshi",
+//     belt: "Black",
+//     age: 30
+// };
+// // ninja.age = "30";
+// ninja.age = 30;
+// ninja.name = "Ryu";
+// // ninja.skills = ["fighting"];
+// // ninja = [];
+// // ninja = "";
+// // ninja = {
+// //     name: "Ryu",
+// //     belt: "Orange"
+// // }
 // ninja = {
 //     name: "Ryu",
 //     belt: "Yellow",
-//     age: 25,
-//     skills: ["fighting"]
+//     age: 25 
 // }
+// // ninja = {
+// //     name: "Ryu",
+// //     belt: "Yellow",
+// //     age: 25,
+// //     skills: ["fighting"]
+// // }
+//5
+//explicit type defining
+var character;
+var age;
+var isMale;
+// character=20;
+character = "mario";
+// isMale = 1;
+isMale = true;
+//ARRAY
+var skills;
+// skills = [20,30];
+// skills.push("fighting");
+skills = ["fighting", "running"];
+var friends = [];
+friends.push("Ken");
+//Union types
--- sandbox.ts
+++ sandbox.ts
@@ -18,50 +18,133 @@
 
 //4
 //ARRAYS
-let names = ["luigi", "mario", "ken"];
-// names.push(2);
-// names[0] = 3
-// names = ""
-names.push("powerup");
-names[1] = "ryu";
-// names = [1,2,3,4];
+// let names = ["luigi", "mario", "ken"];
+// // names.push(2);
+// // names[0] = 3
+// // names = ""
+// names.push("powerup");
+// names[1] = "ryu";
+// // names = [1,2,3,4];
 
-let numbers = [1,2,3];
-// numbers.push("ryu");
-// numbers[1] = "ken";
-numbers.push(4);
-numbers[2] = 5
+// let numbers = [1,2,3];
+// // numbers.push("ryu");
+// // numbers[1] = "ken";
+// numbers.push(4);
+// numbers[2] = 5
 
-let mixed = ["ken", 1, "luigi", 2, 3, "jung-li"];
-mixed.push("ken");
-mixed[0] = 0;
-mixed.push(5);
+// let mixed = ["ken", 1, "luigi", 2, 3, "jung-li"];
+// mixed.push("ken");
+// mixed[0] = 0;
+// mixed.push(5);
 
 //Objects
-let ninja = {
-    name: "Yoshi",
-    belt: "Black",
-    age: 30
-};
-// ninja.age = "30";
-ninja.age = 30;
-ninja.name = "Ryu";
-// ninja.skills = ["fighting"];
-// ninja = [];
-// ninja = "";
-// ninja = {
-//     name: "Ryu",
-//     belt: "Orange"
-// }
-ninja = {
-    name: "Ryu",
-    belt: "Yellow",
-    age: 25 
-}
+// let ninja = {
+//     name: "Yoshi",
+//     belt: "Black",
+//     age: 30
+// };
+// // ninja.age = "30";
+// ninja.age = 30;
+// ninja.name = "Ryu";
+// // ninja.skills = ["fighting"];
+// // ninja = [];
+// // ninja = "";
+// // ninja = {
+// //     name: "Ryu",
+// //     belt: "Orange"
+// // }
 // ninja = {
 //     name: "Ryu",
 //     belt: "Yellow",
+//     age: 25 
+// }
+// // ninja = {
+// //     name: "Ryu",
+// //     belt: "Yellow",
+// //     age: 25,
+// //     skills: ["fighting"]
+// // }
+
+//5
+//explicit type defining
+let character:string;
+let age:number;
+let isMale:boolean;
+
+// character=20;
+character="mario";
+
+// isMale = 1;
+isMale = true;
+
+//ARRAY
+let skills:string[];
+// skills = [20,30];
+// skills.push("fighting");
+skills = ["fighting", "running"]
+
+let friends:string[] = [];
+friends.push("Ken");
+
+//Union types
+let mixed: (string|number)[] = [];
+mixed.push(2);
+mixed.push('2');
+// mixed.push(true);
+
+let uid:string|number;
+uid='pf34';
+uid=56
+
+//OBJECT
+let a:object;
+a = {
+    name:"Yoshi",
+    age: 34,
+    belt: "black"
+};
+a = [];
+// a = ""
+
+let b: {
+    name: string,
+    age: number,
+    belt: string
+};
+// b = []
+b = {
+    name:"mario",
+    age:23,
+    belt: "orange"
+};
+// b = {
+//     name: "luigi"
+// }
+// b = {
+//     name: "ken",
 //     age: 25,
-//     skills: ["fighting"]
+//     belt: "yellow",
+//     skills: ["running"]
 // }
 
+class Ninja {
+    name:string;
+    age:number;
+    belt:string;
+
+    constructor(name: string, age:number, belt:string) {
+        this.name = name;
+        this.age = age;
+        this.belt = belt;
+    }
+};
+
+let c:Ninja;
+// c = "hello"
+c = {
+    name:"John",
+    age: 20,
+    belt: "grey"
+}
+
+c = new Ninja("Joe", 24, "white");
