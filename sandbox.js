b0e35ba72468cbb63fd0d81a112d81296cdaea6e
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2021-05-20 08:44:16 +0530

    Array and object types

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,58 @@
-var character = "mario";
-var age = 10;
-var isblackBelt = false;
-// character = true;
-character = "luigi";
-// age = "twenty"
-age = 20;
-// isblackBelt = "yes";
-isblackBelt = true;
-var circumference = function (diameter) { return diameter * Math.PI; };
-// console.log(circumference("hello"));
-console.log(circumference(7.5));
+//3
+// let character = "mario";
+// let age = 10;
+// let isblackBelt = false;
+// // character = true;
+// character = "luigi";
+// // age = "twenty"
+// age = 20;
+// // isblackBelt = "yes";
+// isblackBelt = true;
+// const circumference = (diameter: number) => diameter * Math.PI;
+// // console.log(circumference("hello"));
+// console.log(circumference(7.5));
+//4
+//ARRAYS
+var names = ["luigi", "mario", "ken"];
+// names.push(2);
+// names[0] = 3
+// names = ""
+names.push("powerup");
+names[1] = "ryu";
+// names = [1,2,3,4];
+var numbers = [1, 2, 3];
+// numbers.push("ryu");
+// numbers[1] = "ken";
+numbers.push(4);
+numbers[2] = 5;
+var mixed = ["ken", 1, "luigi", 2, 3, "jung-li"];
+mixed.push("ken");
+mixed[0] = 0;
+mixed.push(5);
+//Objects
+var ninja = {
+    name: "Yoshi",
+    belt: "Black",
+    age: 30
+};
+// ninja.age = "30";
+ninja.age = 30;
+ninja.name = "Ryu";
+// ninja.skills = ["fighting"];
+// ninja = [];
+// ninja = "";
+// ninja = {
+//     name: "Ryu",
+//     belt: "Orange"
+// }
+ninja = {
+    name: "Ryu",
+    belt: "Yellow",
+    age: 25
+};
+// ninja = {
+//     name: "Ryu",
+//     belt: "Yellow",
+//     age: 25,
+//     skills: ["fighting"]
+// }
